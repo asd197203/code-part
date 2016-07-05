0315eb31660cdff2f5f18d6d761c798e7fb57c97
asd197203 <lxx@lxxdeMacBook-Pro.local>
2016-07-05 09:03:23 +0800

    添加工程

---
+++ CodePart/CodePart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodePart
+//
+//  Created by lxx on 16/7/5.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodePart/CodePart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodePart
+//
+//  Created by lxx on 16/7/5.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
